41f5ddf062b336cbafe9f2d97dce85c4988c5835
anil7620 <54389022+anil7620@users.noreply.github.com>
2024-03-23 14:08:58 -0500

    dbt deploy

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,19 @@
 from flask import Flask
 from flask_pymongo import PyMongo
 from flask_jwt_extended import JWTManager
+import os
+import certifi
 
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/appointment_management"
+app.config["MONGO_URI"] = "mongodb+srv://axk68420:JGWE4RdbICzCBjNV@cluster0.x6ffwl7.mongodb.net/appointment_management"
 # app.config['JWT_SECRET_KEY'] = 'anil'  # Change this!
 # jwt = JWTManager(app)
-
+ca = certifi.where()
 app.secret_key = 'adb'
-mongo = PyMongo(app)
+
+
+mongo = PyMongo(app, tlsCAFile=ca)
 
 # def initialize_routes():
 #     from app.routes import auth_routes, profile_routes
--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from app.routes import auth_routes, profile_routes, default_routes, appointment_
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+     app.run(host='0.0.0.0', port=5000, debug=True)
